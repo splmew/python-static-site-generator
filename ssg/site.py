5d77b16d9b14741f45e35ae4641c5b6fb130c80b
splmew <mwozniko@gmail.com>
2022-12-13 13:22:14 +0100

    site class

---
+++ ssg/site.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        for path in self.source.rglob("*"):
+            self.create_dir(path)
